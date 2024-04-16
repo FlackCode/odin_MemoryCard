3d87fba42869d1829dbaaa81a277dcb5a4155501
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-16 17:52:41 +0200

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <div>
+            <header className="p-2 bg-gray-100 shadow-md">
+                <h1 className="text-xl font-bold">Flack's Memory Game</h1>
+                <p className="text-base font-semibold">Get points by clicking on an image, don't click on any more than once!</p>
+            </header>
+        </div>
+    )
+}
